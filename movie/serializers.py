3709c27c782346216a5ec016dc7a23d7e8e95474
Maruf995 <82045849+Maruf995@users.noreply.github.com>
2021-12-30 20:28:43 +0600

    Update serializers.py

--- movie/serializers.py
+++ movie/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from movie.models import Movie, Review, Genre, Cinema
+from movie.models import Movie, Review, Genre
 
 
 class ReviewListSerializer(serializers.ModelSerializer):
