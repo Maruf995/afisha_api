51193956ee205e72a43b10dc38c0c78c56eb555c
Maruf995 <82045849+Maruf995@users.noreply.github.com>
2021-12-30 18:49:05 +0600

    first commit

--- movie/models.py
+++ movie/models.py
@@ -15,19 +15,19 @@ class Genre(models.Model):
         return self.name
 
 
+class Review(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
     genres = models.ManyToManyField(Genre)
+    reviews = models.ManyToManyField(Review, related_name="reviews")
 
     def __str__(self):
         return self.title
-
-
-class Review(models.Model):
-    text = models.TextField()
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.text
--- movie/serializers.py
+++ movie/serializers.py
@@ -1,8 +1,29 @@
 from rest_framework import serializers
-from movie.models import Movie
+from movie.models import Movie, Review, Genre, Cinema
 
+
+class ReviewListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = 'text'.split()
+
+
+class GenreListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = 'id name'.split()
 
 class MovieListSerializer(serializers.ModelSerializer):
+    reviews = serializers.SerializerMethodField()
+    genres = serializers.SerializerMethodField()
+
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'description', 'cinema', ]
+        fields = 'id title description cinema genres reviews'.split()
+
+    def get_reviews(self, obj):
+        return ReviewListSerializer(obj.reviews, many=True).data
+
+    def get_genres(self, obj):
+        return GenreListSerializer(obj.genres, many=True).data
+
